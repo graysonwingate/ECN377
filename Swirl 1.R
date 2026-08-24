87c5a83c422ff2bcdea5021828e8b1ea66b1d21c
graysonwingate <jgw1258@uncw.edu>
2026-08-24 12:18:26 -0400

    Add files via upload

---
+++ Swirl 1.R
@@ -0,0 +1,48 @@
+library(swirl)
+install_course_github("isaaccloh", "377_swirl")
+
+swirl()
+wingateg
+# select course
+1
+# select lesson
+1
+1
+1
+
+# Q1
+5 + 7
+
+# Q2
+x <- 5 + 7
+
+# Q3
+x
+
+# Q4
+y <- x - 3
+
+# Q4
+y
+
+# Q5
+z <- c(1.1, 9, 3.14)
+
+# Q6
+?
+
+# Q7
+z
+
+# Q8
+c(z, 555, z)
+
+# Q9
+z * 2 + 100
+
+# Q10
+my_sqrt <- sqrt(z - 1)
+
+1
+
+bye()
