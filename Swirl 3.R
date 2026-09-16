c94829cd74508aadcf07bf0ec16647687355a5e5
graysonwingate <jgw1258@uncw.edu>
2026-09-16 11:52:15 -0400

    Add files via upload

---
+++ Swirl 3.R
@@ -0,0 +1,52 @@
+swirl()
+1
+3
+# 1
+1:20
+
+# 2
+pi:10
+
+# 3 
+15:1
+?`:`
+
+# 4
+
+# 5 
+seq(1, 20)
+
+# 6
+seq(0, 10, by=0.5)
+
+# 
+my_seq(5, 10, length=30)
+
+# 7 
+my_seq <- seq(5, 10, length=30)
+
+# 8
+legnth(my_seq)
+
+# 9
+length(my_seq)
+
+# 10
+1:length(my_seq)
+
+# 11
+seq(along.with = my_seq)
+
+# 12
+seq_along(my_seq)
+
+# 13
+rep(0, times = 40)
+
+#14
+rep(c(0, 1, 2), times = 10)
+
+# 15
+rep(c(0,1,2), each = 10)
+
+2
