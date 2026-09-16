d5356757047ff63f011e8e69aff6fb3ddaa129d3
graysonwingate <jgw1258@uncw.edu>
2026-09-16 10:10:52 -0400

    Add files via upload

---
+++ Swirl 4.R
@@ -0,0 +1,59 @@
+swirl()
+wingateg
+2
+1
+4
+
+#1
+num_vect <- c(0.5, 55, -10, 6)
+
+#2
+tf <- num_vect < 1
+
+#3
+1
+
+#4
+tf
+
+#5
+num_vect >= 6
+
+#6
+1
+
+#7
+2
+
+#8
+2
+
+#9
+my_char <- c("My", "name", "is")
+
+#10
+my_char
+
+#11
+paste(my_char, collapse = " ")
+
+#12
+my_name <- c(my_char, "wingateg")
+
+#13
+my_name
+
+#14
+paste(my_name, collapse = " ")
+
+#15
+paste("Hello", "world!", sep = " ")
+
+#16
+paste(1:3, c("X", "Y", "Z"), sep = "")
+
+#17
+paste(LETTERS, 1:4, sep = "-")
+
+#18 
+2
