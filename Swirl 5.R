6b19f0d578ea1646cf93dec267ae121d8942807f
graysonwingate <jgw1258@uncw.edu>
2026-09-16 10:11:01 -0400

    Add files via upload

---
+++ Swirl 5.R
@@ -0,0 +1,42 @@
+2
+1
+5
+1
+
+# 1 
+x <- c(44, NA, 5, NA)
+
+# 2
+3 * x
+
+# 3
+y <- rnorm(1000)
+
+# 4
+z <- rep(NA, 1000)
+
+# 5
+my_data <- sample(c(y, z), 100)
+
+# 6
+my_na <- is.na(my_data)
+
+# 7 
+my_na
+
+# 8 
+my_data == NA
+
+# 9 
+sum(my_na)
+
+# 10 
+my_data
+
+# 11
+0 / 0
+
+# 12
+Inf - Inf
+
+2
